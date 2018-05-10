b91a38213f6a6391bc862c5b282081cc384b5e64
Estelle Sillari-Owen <emso18@gmail.com>
2018-05-10 10:15:43 -0600

    basic working solution

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const multer = require('multer');
+const upload = multer();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,3 +9,9 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(port);
 console.log(`Now listening on port ${port}`);
+
+app.post('/file-size', upload.single('sizeCheck'), function(req, res){
+  res.send({
+    size: req.file.size
+  });
+});
